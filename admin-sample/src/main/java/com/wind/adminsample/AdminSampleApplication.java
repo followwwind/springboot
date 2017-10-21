5f9545a859e2b24ca8b3f861268465c3b31e0b31
followwwind <18771933975@163.com>
2017-10-21 10:58:01 +0800

    修改spring-admin模块配置

--- admin-sample/src/main/java/com/wind/adminsample/AdminSampleApplication.java
+++ admin-sample/src/main/java/com/wind/adminsample/AdminSampleApplication.java
@@ -2,12 +2,15 @@ package com.wind.adminsample;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author wind
  */
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableAdminServer
 public class AdminSampleApplication {
 
