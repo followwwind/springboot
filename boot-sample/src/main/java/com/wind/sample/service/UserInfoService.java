3ba407bfc59547acf72c7b3d1315f5bf528b66b5
followwwind <18771933975@163.com>
2018-09-14 16:11:02 +0800

    清除不必要的代码

--- boot-sample/src/main/java/com/wind/sample/service/UserInfoService.java
+++
@@ -1,7 +0,0 @@
-package com.wind.sample.service;
-
-import com.wind.sample.base.IBaseService;
-import com.wind.sample.entity.UserInfo;
-
-public interface IUserInfoService extends IBaseService<UserInfo, Long> {
-}
